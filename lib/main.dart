d3773bb6a8e0e58d16742a81de8e98c10bacae8d
Vedant Gadekar <vedantgadekar77@gmail.com>
2024-06-24 12:15:06 +0530

    App Completed2.0

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BottomNavbar(), //check
+      home: LogIn(), //check
     );
   }
 }
